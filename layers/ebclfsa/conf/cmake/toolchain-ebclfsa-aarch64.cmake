4b1a7a97304ded88435857f65b146e59fa41084c
Victor Diaconu <victor.diaconu@aox.de>
2025-01-23 10:54:06 +0100

    ebclfsa-layer: update CMake config files
    Edited the CMake JSON configuration files to replace the default GCC-based build system with the following:
      Clang as the compiler
      LLD as the linker
      musl as the standard C library

--- layers/ebclfsa/conf/cmake/toolchain-ebclfsa-aarch64.cmake
+++ layers/ebclfsa/conf/cmake/toolchain-ebclfsa-aarch64.cmake
@@ -1,13 +1,18 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER aarch64-linux-musl-gcc)
-set(CMAKE_CXX_COMPILER aarch64-linux-musl-g++)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
 set(CMAKE_SYSROOT /build/sysroot_hi_aarch64)
 
 # Only static binaries are allowed
-set(CMAKE_C_FLAGS -static)
+set(CMAKE_C_FLAGS "-static")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -isystem /usr/include/aarch64-linux-musl/ -isystem /usr/lib/gcc-cross/aarch64-linux-gnu/11/include/")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -target aarch64-linux-musl")
+
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -nostdlib -L/usr/lib/aarch64-linux-musl -L/usr/lib/gcc-cross/aarch64-linux-gnu/11 -lc -lgcc")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /usr/lib/aarch64-linux-musl/crt1.o /usr/lib/aarch64-linux-musl/crti.o /usr/lib/aarch64-linux-musl/crtn.o /usr/lib/gcc-cross/aarch64-linux-gnu/11/crtend.o /usr/lib/gcc-cross/aarch64-linux-gnu/11/crtbeginT.o")
 
 set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
